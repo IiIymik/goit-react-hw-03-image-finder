4cf3eaa8dacdccddad95eff34db070d559d9aa57
IiIymik <sambotemp@gmail.com>
2021-07-09 16:36:45 +0300

    add file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from './App.styled.js';
+
+export class App extends Component {
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    )
+  }
+}
+
+export default App
---
+++ src/constants/apiKey.js
@@ -0,0 +1,3 @@
+export const API_KEY = '21591796-78fee80e066dbc97e90cc10ea';
+
+
